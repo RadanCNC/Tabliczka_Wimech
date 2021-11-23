8064463039d65f66a2dff8c84c4714d410cfd235
Łukasz Soroka <lsoroka@verashape.com>
2021-11-23 12:38:03 +0100

    zmiana 1

--- DXF To Nest/Tabliczka_Wimech.Designer.vb
+++ DXF To Nest/Tabliczka_Wimech.Designer.vb
@@ -55,198 +55,221 @@ Partial Class Tabliczka_Wimech
         '
         'btnNie
         '
-        Me.btnNie.Location = New System.Drawing.Point(271, 380)
+        Me.btnNie.Location = New System.Drawing.Point(361, 468)
+        Me.btnNie.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.btnNie.Name = "btnNie"
-        Me.btnNie.Size = New System.Drawing.Size(98, 23)
+        Me.btnNie.Size = New System.Drawing.Size(131, 28)
         Me.btnNie.TabIndex = 1
         Me.btnNie.Text = "Anuluj"
         Me.btnNie.UseVisualStyleBackColor = True
         '
         'TBLinia1
         '
-        Me.TBLinia1.Location = New System.Drawing.Point(129, 57)
+        Me.TBLinia1.Location = New System.Drawing.Point(172, 70)
+        Me.TBLinia1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TBLinia1.Name = "TBLinia1"
-        Me.TBLinia1.Size = New System.Drawing.Size(240, 20)
+        Me.TBLinia1.Size = New System.Drawing.Size(319, 22)
         Me.TBLinia1.TabIndex = 9
         Me.TBLinia1.Text = "WIMECH"
         '
         'TBLinia2
         '
-        Me.TBLinia2.Location = New System.Drawing.Point(129, 100)
+        Me.TBLinia2.Location = New System.Drawing.Point(172, 123)
+        Me.TBLinia2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TBLinia2.Name = "TBLinia2"
-        Me.TBLinia2.Size = New System.Drawing.Size(240, 20)
+        Me.TBLinia2.Size = New System.Drawing.Size(319, 22)
         Me.TBLinia2.TabIndex = 10
         Me.TBLinia2.Text = "COMBO ANTALYA II"
         '
         'TBLinia3
         '
-        Me.TBLinia3.Location = New System.Drawing.Point(129, 142)
+        Me.TBLinia3.Location = New System.Drawing.Point(172, 175)
+        Me.TBLinia3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TBLinia3.Name = "TBLinia3"
-        Me.TBLinia3.Size = New System.Drawing.Size(240, 20)
+        Me.TBLinia3.Size = New System.Drawing.Size(319, 22)
         Me.TBLinia3.TabIndex = 11
         Me.TBLinia3.Text = "NMK911000/2"
         '
         'TBLinia4Prefix
         '
-        Me.TBLinia4Prefix.Location = New System.Drawing.Point(129, 189)
+        Me.TBLinia4Prefix.Location = New System.Drawing.Point(172, 233)
+        Me.TBLinia4Prefix.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TBLinia4Prefix.Name = "TBLinia4Prefix"
-        Me.TBLinia4Prefix.Size = New System.Drawing.Size(31, 20)
+        Me.TBLinia4Prefix.Size = New System.Drawing.Size(40, 22)
         Me.TBLinia4Prefix.TabIndex = 12
         Me.TBLinia4Prefix.Text = "S/N"
         '
         'TBLinia5
         '
-        Me.TBLinia5.Location = New System.Drawing.Point(129, 231)
+        Me.TBLinia5.Location = New System.Drawing.Point(172, 284)
+        Me.TBLinia5.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TBLinia5.Name = "TBLinia5"
-        Me.TBLinia5.Size = New System.Drawing.Size(240, 20)
+        Me.TBLinia5.Size = New System.Drawing.Size(319, 22)
         Me.TBLinia5.TabIndex = 13
         Me.TBLinia5.Text = "2680/2021"
         '
         'Lb1
         '
         Me.Lb1.AutoSize = True
-        Me.Lb1.Location = New System.Drawing.Point(101, 64)
+        Me.Lb1.Location = New System.Drawing.Point(135, 79)
+        Me.Lb1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Lb1.Name = "Lb1"
-        Me.Lb1.Size = New System.Drawing.Size(13, 13)
+        Me.Lb1.Size = New System.Drawing.Size(16, 17)
         Me.Lb1.TabIndex = 14
         Me.Lb1.Text = "1"
         '
         'Lb2
         '
         Me.Lb2.AutoSize = True
-        Me.Lb2.Location = New System.Drawing.Point(101, 107)
+        Me.Lb2.Location = New System.Drawing.Point(135, 132)
+        Me.Lb2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Lb2.Name = "Lb2"
-        Me.Lb2.Size = New System.Drawing.Size(13, 13)
+        Me.Lb2.Size = New System.Drawing.Size(16, 17)
         Me.Lb2.TabIndex = 15
         Me.Lb2.Text = "2"
         '
         'Lb3
         '
         Me.Lb3.AutoSize = True
-        Me.Lb3.Location = New System.Drawing.Point(101, 149)
+        Me.Lb3.Location = New System.Drawing.Point(135, 183)
+        Me.Lb3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Lb3.Name = "Lb3"
-        Me.Lb3.Size = New System.Drawing.Size(13, 13)
+        Me.Lb3.Size = New System.Drawing.Size(16, 17)
         Me.Lb3.TabIndex = 16
         Me.Lb3.Text = "3"
         '
         'Lb4
         '
         Me.Lb4.AutoSize = True
-        Me.Lb4.Location = New System.Drawing.Point(101, 196)
+        Me.Lb4.Location = New System.Drawing.Point(135, 241)
+        Me.Lb4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Lb4.Name = "Lb4"
-        Me.Lb4.Size = New System.Drawing.Size(13, 13)
+        Me.Lb4.Size = New System.Drawing.Size(16, 17)
         Me.Lb4.TabIndex = 17
         Me.Lb4.Text = "4"
         '
         'Lb5
         '
         Me.Lb5.AutoSize = True
-        Me.Lb5.Location = New System.Drawing.Point(101, 234)
+        Me.Lb5.Location = New System.Drawing.Point(135, 288)
+        Me.Lb5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Lb5.Name = "Lb5"
-        Me.Lb5.Size = New System.Drawing.Size(13, 13)
+        Me.Lb5.Size = New System.Drawing.Size(16, 17)
         Me.Lb5.TabIndex = 18
         Me.Lb5.Text = "5"
         '
         'LblLinia
         '
         Me.LblLinia.AutoSize = True
-        Me.LblLinia.Location = New System.Drawing.Point(85, 22)
+        Me.LblLinia.Location = New System.Drawing.Point(113, 27)
+        Me.LblLinia.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.LblLinia.Name = "LblLinia"
-        Me.LblLinia.Size = New System.Drawing.Size(29, 13)
+        Me.LblLinia.Size = New System.Drawing.Size(38, 17)
         Me.LblLinia.TabIndex = 19
         Me.LblLinia.Text = "Linia"
         '
         'LblTekst
         '
         Me.LblTekst.AutoSize = True
-        Me.LblTekst.Location = New System.Drawing.Point(340, 22)
+        Me.LblTekst.Location = New System.Drawing.Point(453, 27)
+        Me.LblTekst.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.LblTekst.Name = "LblTekst"
-        Me.LblTekst.Size = New System.Drawing.Size(34, 13)
+        Me.LblTekst.Size = New System.Drawing.Size(43, 17)
         Me.LblTekst.TabIndex = 20
         Me.LblTekst.Text = "Tekst"
         '
         'BtnZapiszCzesci
         '
-        Me.BtnZapiszCzesci.Location = New System.Drawing.Point(271, 342)
+        Me.BtnZapiszCzesci.Location = New System.Drawing.Point(361, 421)
+        Me.BtnZapiszCzesci.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.BtnZapiszCzesci.Name = "BtnZapiszCzesci"
-        Me.BtnZapiszCzesci.Size = New System.Drawing.Size(98, 23)
+        Me.BtnZapiszCzesci.Size = New System.Drawing.Size(131, 28)
         Me.BtnZapiszCzesci.TabIndex = 21
         Me.BtnZapiszCzesci.Text = "Stwórz tabliczki"
         Me.BtnZapiszCzesci.UseVisualStyleBackColor = True
         '
         'TBSciezkaDoZapisu
         '
-        Me.TBSciezkaDoZapisu.Location = New System.Drawing.Point(129, 283)
+        Me.TBSciezkaDoZapisu.Location = New System.Drawing.Point(172, 348)
+        Me.TBSciezkaDoZapisu.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TBSciezkaDoZapisu.Name = "TBSciezkaDoZapisu"
-        Me.TBSciezkaDoZapisu.Size = New System.Drawing.Size(240, 20)
+        Me.TBSciezkaDoZapisu.Size = New System.Drawing.Size(319, 22)
         Me.TBSciezkaDoZapisu.TabIndex = 22
         Me.TBSciezkaDoZapisu.Text = "D:\"
         '
         'LbSciezkaDozapisu
         '
         Me.LbSciezkaDozapisu.AutoSize = True
-        Me.LbSciezkaDozapisu.Location = New System.Drawing.Point(30, 286)
+        Me.LbSciezkaDozapisu.Location = New System.Drawing.Point(40, 352)
+        Me.LbSciezkaDozapisu.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.LbSciezkaDozapisu.Name = "LbSciezkaDozapisu"
-        Me.LbSciezkaDozapisu.Size = New System.Drawing.Size(93, 13)
+        Me.LbSciezkaDozapisu.Size = New System.Drawing.Size(122, 17)
         Me.LbSciezkaDozapisu.TabIndex = 23
         Me.LbSciezkaDozapisu.Text = "Ścieżka do zapisu"
         '
         'TBLinia4Rok
         '
-        Me.TBLinia4Rok.Location = New System.Drawing.Point(166, 189)
+        Me.TBLinia4Rok.Location = New System.Drawing.Point(221, 233)
+        Me.TBLinia4Rok.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TBLinia4Rok.Name = "TBLinia4Rok"
-        Me.TBLinia4Rok.Size = New System.Drawing.Size(32, 20)
+        Me.TBLinia4Rok.Size = New System.Drawing.Size(41, 22)
         Me.TBLinia4Rok.TabIndex = 24
         Me.TBLinia4Rok.Text = "2021"
         '
         'TBLinia4Od
         '
-        Me.TBLinia4Od.Location = New System.Drawing.Point(258, 189)
+        Me.TBLinia4Od.Location = New System.Drawing.Point(344, 233)
+        Me.TBLinia4Od.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TBLinia4Od.Name = "TBLinia4Od"
-        Me.TBLinia4Od.Size = New System.Drawing.Size(40, 20)
+        Me.TBLinia4Od.Size = New System.Drawing.Size(52, 22)
         Me.TBLinia4Od.TabIndex = 25
         Me.TBLinia4Od.Text = "00000"
         '
         'TBLinia4Do
         '
-        Me.TBLinia4Do.Location = New System.Drawing.Point(329, 189)
+        Me.TBLinia4Do.Location = New System.Drawing.Point(439, 233)
+        Me.TBLinia4Do.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TBLinia4Do.Name = "TBLinia4Do"
-        Me.TBLinia4Do.Size = New System.Drawing.Size(40, 20)
+        Me.TBLinia4Do.Size = New System.Drawing.Size(52, 22)
         Me.TBLinia4Do.TabIndex = 26
         Me.TBLinia4Do.Text = "00005"
         '
         'Label11
         '
         Me.Label11.AutoSize = True
-        Me.Label11.Location = New System.Drawing.Point(233, 192)
+        Me.Label11.Location = New System.Drawing.Point(311, 236)
+        Me.Label11.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label11.Name = "Label11"
-        Me.Label11.Size = New System.Drawing.Size(19, 13)
+        Me.Label11.Size = New System.Drawing.Size(24, 17)
         Me.Label11.TabIndex = 27
         Me.Label11.Text = "od"
         '
         'Label12
         '
         Me.Label12.AutoSize = True
-        Me.Label12.Location = New System.Drawing.Point(304, 192)
+        Me.Label12.Location = New System.Drawing.Point(405, 236)
+        Me.Label12.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label12.Name = "Label12"
-        Me.Label12.Size = New System.Drawing.Size(19, 13)
+        Me.Label12.Size = New System.Drawing.Size(24, 17)
         Me.Label12.TabIndex = 28
         Me.Label12.Text = "do"
         '
         'TBNumerPiora
         '
-        Me.TBNumerPiora.Location = New System.Drawing.Point(104, 383)
+        Me.TBNumerPiora.Location = New System.Drawing.Point(139, 471)
+        Me.TBNumerPiora.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TBNumerPiora.Name = "TBNumerPiora"
-        Me.TBNumerPiora.Size = New System.Drawing.Size(45, 20)
+        Me.TBNumerPiora.Size = New System.Drawing.Size(59, 22)
         Me.TBNumerPiora.TabIndex = 29
         Me.TBNumerPiora.Text = "1"
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 386)
+        Me.Label1.Location = New System.Drawing.Point(16, 475)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(65, 13)
+        Me.Label1.Size = New System.Drawing.Size(87, 17)
         Me.Label1.TabIndex = 30
         Me.Label1.Text = "Numer Pióra"
         '
@@ -254,60 +277,66 @@ Partial Class Tabliczka_Wimech
         '
         Me.ComboBox1.FormattingEnabled = True
         Me.ComboBox1.Items.AddRange(New Object() {".dxf", ".sym"})
-        Me.ComboBox1.Location = New System.Drawing.Point(129, 342)
+        Me.ComboBox1.Location = New System.Drawing.Point(172, 421)
+        Me.ComboBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.Size = New System.Drawing.Size(160, 24)
         Me.ComboBox1.TabIndex = 31
         Me.ComboBox1.Text = ".sym"
         '
         'TextCzcionka
         '
-        Me.TextCzcionka.Location = New System.Drawing.Point(104, 418)
+        Me.TextCzcionka.Location = New System.Drawing.Point(139, 514)
+        Me.TextCzcionka.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TextCzcionka.Name = "TextCzcionka"
-        Me.TextCzcionka.Size = New System.Drawing.Size(146, 20)
+        Me.TextCzcionka.Size = New System.Drawing.Size(193, 22)
         Me.TextCzcionka.TabIndex = 32
         Me.TextCzcionka.Text = "Laser sans Serif black"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(12, 421)
+        Me.Label2.Location = New System.Drawing.Point(16, 518)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(82, 13)
+        Me.Label2.Size = New System.Drawing.Size(104, 17)
         Me.Label2.TabIndex = 33
         Me.Label2.Text = "Nazwa czcionki"
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(129, 316)
+        Me.TextBox1.Location = New System.Drawing.Point(172, 389)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(45, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(59, 22)
         Me.TextBox1.TabIndex = 34
         Me.TextBox1.Text = "0,05"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(37, 319)
+        Me.Label3.Location = New System.Drawing.Point(49, 393)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(57, 13)
+        Me.Label3.Size = New System.Drawing.Size(75, 17)
         Me.Label3.TabIndex = 35
         Me.Label3.Text = "Tolerancja"
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(552, 196)
+        Me.Button1.Location = New System.Drawing.Point(736, 241)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.Size = New System.Drawing.Size(100, 28)
         Me.Button1.TabIndex = 36
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "Zmiana"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Tabliczka_Wimech
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(707, 450)
+        Me.ClientSize = New System.Drawing.Size(943, 554)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.TextBox1)
@@ -338,6 +367,7 @@ Partial Class Tabliczka_Wimech
         Me.Controls.Add(Me.TBLinia1)
         Me.Controls.Add(Me.btnNie)
         Me.Location = New System.Drawing.Point(250, 500)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "Tabliczka_Wimech"
         Me.Text = "Kreator Tabliczek"
         Me.ResumeLayout(False)
